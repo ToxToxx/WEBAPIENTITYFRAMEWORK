6e54c58ad9d2ff235b7677c3c0bb7c7ae93523ee
Matvey <sinema3d00@gmail.com>
2024-08-19 13:42:34 +0300

    try controller

---
+++ WEBAPIENTITYFRAMEWORK/Controllers/SuperHeroController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEBAPIENTITYFRAMEWORK.Entities;
+
+
+namespace WEBAPIENTITYFRAMEWORK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham",
+                }
+            };
+
+            return Ok(heroes);
+        }
+    }
+}
